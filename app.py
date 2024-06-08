ca2425bd117580272a3fcf10a8e7ccb452f0ad9f
Pric3 <minhnhatbg05@gmail.com>
2024-06-08 21:15:48 +0700

    Add docker and json

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+# add future value and present value
 
 app = Flask(__name__)
 
@@ -6,6 +9,22 @@ app = Flask(__name__)
 expenses = []
 budget = 0
 balance = 0
+
+def save_to_json():
+    with open('data.json', 'w') as f:
+        json.dump({expenses: expenses, budget: budget, balance: balance}, f)
+
+def load_from_json():
+    if os.path.exists('data.json') and os.path.getsize('data.json') > 0:
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+            expenses = data['expenses']
+            budget = data['budget']
+            balance = data['balance']
+    else:
+        expenses = []
+        budget = 0
+        balance = 0
 
 @app.route('/')
 def index():
@@ -17,6 +36,7 @@ def index():
 def set_budget():
     global budget
     budget = int(request.form['budget'])
+    save_to_json()
     return redirect(url_for('index'))
 
 @app.route('/add_expense', methods=['POST'])
@@ -26,7 +46,9 @@ def add_expense():
         'amount': int(request.form['expense_amount'])
     }
     expenses.append(expense)
+    save_to_json()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    load_from_json()
     app.run(host='0.0.0.0', port='5000', debug=True)
